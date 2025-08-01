5f4999545568faf03c7acb1f811dddf62707b9ef
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-08-01 11:47:08 -0400

    enhanced images didnt work out

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 	build: {
 		target: "es2015"
 	}
