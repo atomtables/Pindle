b92d83590fc4de747feacf8fd579303827a960c7
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-08-03 22:48:25 -0400

    fix cors

--- database/index.js
+++ database/index.js
@@ -43,7 +43,7 @@ function censorBadWords(name) {
 }
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://pindle.atomtables.dev');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
